34fd6a5e9dabb01c3f46e914b3b394a13d8f7702
Jordi <67053157+JordiMesas@users.noreply.github.com>
2020-10-29 15:25:17 +0100

    finished practice2

--- db/gestion_de_notas.sql
+++ db/gestion_de_notas.sql
@@ -1,9 +1,10 @@
+CREATE DATABASE gestion_de_notas;
 -- phpMyAdmin SQL Dump
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2020 at 11:38 AM
+-- Generation Time: Oct 29, 2020 at 03:13 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -38,10 +39,10 @@ CREATE TABLE `administrador` (
 --
 
 INSERT INTO `administrador` (`id`, `email`, `passwd`) VALUES
-(1, 'random@gmail.com', '1234'),
-(2, 'david@gmail.com', '1234'),
-(3, 'pere@gmail.com', '1234'),
-(4, 'montse@gmail.com', '1234');
+(1, 'random@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(2, 'david@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(3, 'pere@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(4, 'montse@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
 
 -- --------------------------------------------------------
 
@@ -64,8 +65,8 @@ CREATE TABLE `alumnos` (
 --
 
 INSERT INTO `alumnos` (`id`, `nombre`, `apellido paterno`, `apellido materno`, `grupo`, `email`, `passwd`) VALUES
-(1, 'david', 'mesas', 'del rio', 'daw2', 'david@gmail.com', '1234'),
-(2, 'Jordi', 'mesas', 'del rio', 'daw2', 'jordi_mr90@hotmail.com', '1234');
+(8, 'David', 'Mesas', 'del Rio', 'Daw2', 'david@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(9, 'Jordi', 'mafias', 'Palotes', 'daw2', 'jordi@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
 
 -- --------------------------------------------------------
 
@@ -85,12 +86,12 @@ CREATE TABLE `notas` (
 --
 
 INSERT INTO `notas` (`id`, `nombre_de_asignatura`, `id_alumno`, `nota`) VALUES
-(1, 'mates', 1, 4),
-(2, 'fisica', 1, 4),
-(3, 'programacion', 1, 4),
-(7, 'mates', 2, 8),
-(8, 'fisica', 2, 8),
-(9, 'programacion', 2, 8);
+(16, 'mates', 8, 9),
+(17, 'fisica', 8, 5),
+(18, 'programacion', 8, 2),
+(19, 'mates', 9, 3),
+(20, 'fisica', 9, 7),
+(21, 'programacion', 9, 9);
 
 --
 -- Indexes for dumped tables
@@ -129,13 +130,13 @@ ALTER TABLE `administrador`
 -- AUTO_INCREMENT for table `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `notas`
 --
 ALTER TABLE `notas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
